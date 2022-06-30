89ac24f24f02b97d6270cc353bec6e6f41b4d67d
amKex <amitbiswas53467@gmail.com>
2022-06-30 21:30:23 +0530

    this is a tamplate to find peak in a mountain arrey

---
+++ Peak Index in a Mountain Array.py
@@ -0,0 +1,13 @@
+#arr = [0, 10, 50, 2]
+
+def fun(arr):
+    left = 0 
+    right = len(arr) - 1
+    while left < right :
+        mid = (left + right) // 2
+        if arr[mid] < arr[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+print(fun([0, 10, 50, 2]))
